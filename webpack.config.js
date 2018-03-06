a3b9c8d025f0f363e3f6c2cfbcd7d4b836faed52
Khachatour <loledgamer96@hotmail.com>
2018-03-06 14:04:38 +0400

    core: new modules, ready webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
-module.export = {
-  entry: "./src/index"
-}
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src",
+  output: {
+    path: __dirname + "/dist",
+    filename: "khchos_greatest_bundle.js"
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
