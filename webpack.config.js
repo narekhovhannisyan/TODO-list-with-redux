1f619f982abae2de3887c7329c69a341727a0fd1
narekhovhannisyan <narekgilmour@gmail.com>
2018-03-06 18:45:14 +0400

    config: configure babel and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src",
+  entry: './src/root',
   output: {
-    path: __dirname + "/dist",
-    filename: "source.js",
+    path: __dirname + '/dist',
+    filename: 'source.js',
   },
-  plugins: [new HtmlWebpackPlugin()]
-};
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve('./src/root/index.html'),
+    filename: 'index.html',
+    inject: 'body'
+  })]
+}
