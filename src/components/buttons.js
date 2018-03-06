87f7d68a224603028b4560b24fa260f4e3b75690
narekhovhannisyan <narekgilmour@gmail.com>
2018-03-06 13:21:29 +0400

    root: clean whole project

--- src/components/buttons.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const SubmitButton = (props) => {
-  return (<button onClick={() => props.onClick(props.elemIndex)}>Done</button>)
-}
-
-export const RemoveButton = (props) => {
-  return (<button onClick={() => props.onClick(props.elemIndex)}>Remove</button>)
-}
