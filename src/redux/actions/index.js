514fa422c55fc804311254521eddb1db46065e3a
LilitArz <lilitarzzz@gmail.com>
2018-03-07 18:32:23 +0400

    action: in index.js file change function name
     - change changeTodosState to markAsResolved

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,20 +1,20 @@
 export const addTodo = text => {
   return {
-    type: 'ADD_TODO',
+    type: "ADD_TODO",
     value: text
-  }
-}
+  };
+};
 
 export const removeTodo = index => {
   return {
-    type: 'REMOVE_TODO',
+    type: "REMOVE_TODO",
     value: index
-  }
-}
+  };
+};
 
-export const changeTodosState = index => {
+export const markAsResolved = index => {
   return {
-    type: 'CHANGE_STATE',
+    type: "CHANGE_STATE",
     value: index
-  }
-}
+  };
+};
