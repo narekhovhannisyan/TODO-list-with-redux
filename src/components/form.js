611f25eed6b2804c41a5e25c04590194acc56dc0
LilitArz <lilitarzzz@gmail.com>
2018-03-07 13:21:43 +0400

    src: define actionCreators,mapStateTpProps
     - replace store.dispatch with addTodo function call
     - create list for todos

--- src/components/form.js
+++ src/components/form.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react'
-import { store } from '../redux/store'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { store } from "../redux/store";
+
+import { addTodo } from "../redux/actions";
+
+const actionCreators = dispatch => bindActionCreators({ addTodo }, dispatch);
+
+const mapStateToProps = store => ({
+  todos: store.todos
+});
 
 class Form extends Component {
-  render () {
-    return <div>
-      <input ref={input => this.textInput = input}/>
-      <button onClick={() => store.dispatch({
-        type: 'ADD_TODO',
-        text: this.textInput.value
-      })}
-      >Submit
-      </button>
-    </div>
+  render() {
+    return (
+      <div>
+        <input ref={input => (this.textInput = input)} />
+        <button onClick={() => this.props.addTodo(this.textInput.value)}>
+          Submit
+        </button>
+        <ul>{this.props.todos.map(todo => <li>{todo.text}</li>)}</ul>
+      </div>
+    );
   }
 }
+
+export default connect(mapStateToProps, actionCreators)(Form);
