172556a0ca83863f42d38c14a09f7842d7aec4e7
LilitArz <lilitarzzz@gmail.com>
2018-03-07 18:37:16 +0400

    components: in form.js file add button component

--- src/components/form.js
+++ src/components/form.js
@@ -1,50 +1,64 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
-import { addTodo, removeTodo, markAsResolved } from '../redux/actions'
+import { addTodo, removeTodo, markAsResolved } from "../redux/actions";
 
-import { Button } from './buttons'
+import { Button } from "./buttons";
 
-const actionCreators = dispatch => bindActionCreators({ addTodo, removeTodo, markAsResolved }, dispatch)
+const actionCreators = dispatch =>
+  bindActionCreators({ addTodo, removeTodo, markAsResolved }, dispatch);
 
 const mapStateToProps = store => ({
   todos: store.todos
-})
+});
 
 class Form extends Component {
-  submitTodo () {
-    if (this.textInput.value !== '') {
-      this.props.addTodo(this.textInput.value)
+  submitTodo() {
+    if (this.textInput.value !== "") {
+      this.props.addTodo(this.textInput.value);
     }
 
-    this.textInput.value = ''
+    this.textInput.value = "";
   }
 
-  render () {
+  render() {
     return (
       <div>
-        <input ref={input => (this.textInput = input)}
-               onKeyPress={event => {
-                 if (event.key === 'Enter') {
-                   this.submitTodo()
-                 }
-               }}/>
-        <Button buttonTitle='Submit TODO'
-                onClick={() => this.submitTodo()}
+        <input
+          ref={input => (this.textInput = input)}
+          onKeyPress={event => {
+            if (event.key === "Enter") {
+              this.submitTodo();
+            }
+          }}
         />
-        <ul>{this.props.todos.map((todo, index) =>
-          <div>
-            {todo.completed}
-            <li style={{ textDecoration: 'line-through' }}>{todo.text}</li>
-            <Button buttonTitle='Remove Todo'
-                    onClick={() => this.props.removeTodo(index)}/>
-
-          </div>
-        )}</ul>
+        <Button buttonTitle="Submit TODO" onClick={() => this.submitTodo()} />
+        <ul>
+          {this.props.todos.map((todo, index) => (
+            <div>
+              {todo.completed}
+              <li
+                style={{
+                  textDecoration: todo.completed ? "line-through" : "none"
+                }}
+              >
+                {todo.text}
+              </li>
+              <Button
+                buttonTitle="Remove Todo"
+                onClick={() => this.props.removeTodo(index)}
+              />
+              <Button
+                buttonTitle={todo.completed ? "Resolved" : "Resolve"}
+                onClick={() => this.props.markAsResolved(index)}
+              />
+            </div>
+          ))}
+        </ul>
       </div>
-    )
+    );
   }
 }
 
-export default connect(mapStateToProps, actionCreators)(Form)
+export default connect(mapStateToProps, actionCreators)(Form);
