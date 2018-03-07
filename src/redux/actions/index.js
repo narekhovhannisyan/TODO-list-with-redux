506dc30addd72110722bc27e5a944a5d8698d4c7
narekhovhannisyan <narekgilmour@gmail.com>
2018-03-07 17:51:59 +0400

    actions: add removeTodo and changeTodosState actions

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,20 @@
 export const addTodo = text => {
   return {
-    type: "ADD_TODO",
-    text
-  };
-};
+    type: 'ADD_TODO',
+    value: text
+  }
+}
+
+export const removeTodo = index => {
+  return {
+    type: 'REMOVE_TODO',
+    value: index
+  }
+}
+
+export const changeTodosState = index => {
+  return {
+    type: 'CHANGE_STATE',
+    value: index
+  }
+}
