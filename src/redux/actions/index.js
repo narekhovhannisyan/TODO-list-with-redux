aeddeb7f4b718d45715c6101fdb556d710995163
narekhovhannisyan <narekgilmour@gmail.com>
2018-03-09 14:58:09 +0400

    reducers/actions: lint whole file

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,39 +1,41 @@
 export const addTodo = text => {
-  return {
-    type: "ADD_TODO",
-    value: text
-  };
-};
+  if (text !== '') {
+    return {
+      type: 'ADD_TODO',
+      value: text
+    }
+  }
+}
 
 export const removeTodo = index => {
   return {
-    type: "REMOVE_TODO",
+    type: 'REMOVE_TODO',
     value: index
-  };
-};
+  }
+}
 
 export const markAsResolved = index => {
   return {
-    type: "CHANGE_STATE",
+    type: 'CHANGE_STATE',
     value: index
-  };
-};
+  }
+}
 export const showUnResolved = () => {
   return {
-    type: "FILTER-UNRESOLVED",
-    filter: "UNRESOLVED"
-  };
-};
+    type: 'FILTER_UNRESOLVED',
+    filter: 'UNRESOLVED'
+  }
+}
 
 export const showResolved = () => {
   return {
-    type: "FILTER-RESOLVED",
-    filter: "RESOLVED"
-  };
-};
+    type: 'FILTER_RESOLVED',
+    filter: 'RESOLVED'
+  }
+}
 export const showAll = () => {
   return {
-    type: "SHOW-ALL",
-    filter: "NONE"
-  };
-};
+    type: 'SHOW_ALL',
+    filter: 'NONE'
+  }
+}
